f3b6001f27074d7c341d871eac30e83d253608fe
Andrea Beconcini <andrea.beconcini@gmail.com>
2018-12-12 23:55:54 +0100

    Several fix to project layout

--- puzzle02/src/main.rs
+++ puzzle02/src/main.rs
@@ -2,24 +2,20 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
 
-fn load_input() -> std::io::Result<Vec> {
+fn load_input() -> std::io::Result<Vec<String>> {
     let file = File::open("input")?;
     let buf_reader = BufReader::new(file);
-    let mut v<str> = vec![];
+    let mut v: Vec<String> = vec![];
     for res in buf_reader.lines() {
-        let s = res?;
-        let trimmered = s.trim();
-        v.push_back(s);
+        let s = res.unwrap();
+        let trimmed = String::from(s.trim());
+        v.push(trimmed);
     }
-    v;
+    Ok(v)
 }
 
 fn main() {
-    let v = load_input();
-    match load_input() {
-        Ok(v) => println!("{}", v),
-        _ => ,
-    }
+    let v = load_input().unwrap();
     for s in v {
         println!("{}", s);
     }
